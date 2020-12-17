caa75c716dff98c88d680d04f5ae6c5e9f76904a
rutrut6969 <rutledgeisaac6969@gmail.com>
2020-12-17 00:25:02 -0600

    Having Issues with LESS

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import './CSS/index.css';
+import Body from './Components/Body/Body';
+import Nav from './Components/Nav/Nav';
+function App() {
+  return (
+    <div className='App'>
+      <Nav />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
